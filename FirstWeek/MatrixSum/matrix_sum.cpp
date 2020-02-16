e363432a80b078ee68bc54722d9187fda8ea94da
CheeseCoffee <akbarsky93@mail.ru>
2020-02-17 01:43:54 +0300

    reloaD

--- FirstWeek/MatrixSum/matrix_sum.cpp
+++ FirstWeek/MatrixSum/matrix_sum.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Matrix{
+public:
+    Matrix (){
+    }
+    Matrix (int num_rows,int num_cols){
+    }
+    int GetNumRows(){
+        return numRow;
+    }
+    int GetNumColumns(){
+        return numColumn;
+    }
+    void Reset(int new_numRow,int new_)
+    const At()
+private:
+    vector<int>data;
+    int numRow;
+    int numColumn;
+};
+// Реализуйте здесь
+// * класс Matrix
+// * оператор ввода для класса Matrix из потока istream
+// * оператор вывода класса Matrix в поток ostream
+// * оператор проверки на равенство двух объектов класса Matrix
+// * оператор сложения двух объектов класса Matrix
 
+int main() {
+    Matrix one;
+    Matrix two;
+
+    cin >> one >> two;
+    cout << one + two << endl;
+    return 0;
+}
