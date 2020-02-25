fdd40e65097de3750b721628c16cb5f5093afe4a
CheeseCoffee <akbarsky93@mail.ru>
2020-02-25 23:07:19 +0300

    added templates

---
+++ FirstWeek/Templates/sqr_all.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Akbar on 25.02.2020.
+//
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+using namespace std;
+
+template <typename T> T Sqr(T v){
+    return v*v;
+}
+
+int main(){
+    // Пример вызова функции
+    vector<int> v = {1, 2, 3};
+    cout << "vector:";
+    for (int x : Sqr(v)) {
+        cout << ' ' << x;
+    }
+    cout << endl;
+
+    map<int, pair<int, int>> map_of_pairs = {
+            {4, {2, 2}},
+            {7, {4, 3}}
+    };
+    cout << "map of pairs:" << endl;
+    for (const auto& x : Sqr(map_of_pairs)) {
+        cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+    }
+    return 0;
+}
